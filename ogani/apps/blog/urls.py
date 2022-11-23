8118abffbbc3ee889a7126a19c09156a6f77d2b8
Husan2000 <102695862+Husan2000@users.noreply.github.com>
2022-11-24 01:10:10 +0500

    Delete ogani directory

--- ogani/apps/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', blog, name='blog'),
-    path('blog-details/<int:pk>', blog_details, name='blog-details'),
-]
